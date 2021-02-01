efe8a1c54f2e898deaaba27cdf3815dd5c51a4ec
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 17:13:34 -0600

    render props

--- components/src/index.js
+++ components/src/index.js
@@ -5,9 +5,9 @@ import CommentDetails from "./CommentDetails"
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetails author={"Miky"}/>
-            <CommentDetails author={"Arianna"}/>
-            <CommentDetails author={"Julie"}/>
+            <CommentDetails author="Miky" />
+            <CommentDetails author="Arianna"/>
+            <CommentDetails author="Julie"/>
         </div>
     );
 }
