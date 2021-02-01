a01854667cc985bf7c8aca4c990b1ca21953c714
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 16:45:47 -0600

    using ne component

--- components/src/CommentDetails.js
+++ components/src/CommentDetails.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const CommentDetails = () => {
     return (
-        <div>
-            
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img
+            alt="avatar"
+            src="https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png"
+          ></img>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6 p.m</span>
+          </div>
+          <div className="text">Nice Blog Post</div>
         </div>
-    )
+      </div>
+    );
 }
 
-export default CommentDetails
+export default CommentDetails;
