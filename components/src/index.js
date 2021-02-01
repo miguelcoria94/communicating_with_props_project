34297f76f220a15ac107f966999e320b41120017
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 15:56:44 -0600

    made a comment

--- components/src/index.js
+++ components/src/index.js
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     return (
-        <div>Hi there!</div>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"></img>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            Today at 6 p.m
+                        </span>
+                    </div>
+                    <div className="text">Nice Blog Post</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
