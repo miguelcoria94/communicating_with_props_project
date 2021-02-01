7501377fc592c6056a00b775baf9eb6301a21628
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-01 15:21:15 -0600

    set up preoject

---
+++ components/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>H there!</div>
+    )
+}
+
+ReactDOM.render(<App/>, document.querySelector("#root"))
